9249cc9c50732d39757f6b81d5f6771810c6fe29
nextgendev2029 <tuhinrock121@gmail.com>
2025-11-27 17:53:31 +0530

    Add budget module - basic set and get operations

---
+++ js/modules/budgets.js
@@ -0,0 +1,26 @@
+// Budget Manager - Handles budget operations
+export class BudgetManager {
+    constructor(storage) {
+        this.storage = storage;
+        this.budgets = {};
+    }
+
+    loadFromStorage() {
+        this.budgets = this.storage.get('budgets') || {};
+    }
+
+    saveToStorage() {
+        this.storage.set('budgets', this.budgets);
+    }
+
+    set(category, limit) {
+        this.budgets[category] = limit;
+        this.saveToStorage();
+    }
+
+    get(category) {
+        return this.budgets[category] || null;
+    }
+
+    
+}
